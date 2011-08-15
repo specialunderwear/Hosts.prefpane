d4e9de724a10a31f29dd25aa28b482323382e93b
Lars van de Kerkhof <lars@permanentmarkers.nl>
2011-08-15 07:37:03 +0200

    model files now have the correct license.

--- pm/models/NLPERMANENTMARKERSHOSTSHostEntry.h
+++ pm/models/NLPERMANENTMARKERSHOSTSHostEntry.h
@@ -1,10 +1,20 @@
+//    Hosts, a system preference pane to manage your hosts file.
+//    Copyright (C) 2011  PermanentMarkers
 //
-//  OBHostEntry.h
-//  Hosts
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
 //
-//  Created by ebone on 13-03-11.
-//  Copyright 2011 PermanentMarkers. All rights reserved.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 //
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    contact maintainer at hosts@permanentmarkers.nl
 
 #import <Cocoa/Cocoa.h>
 #import "c/parser/hosts.h"
